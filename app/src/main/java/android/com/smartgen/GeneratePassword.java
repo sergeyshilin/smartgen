c53268154e286e46f00d03a4b7e232cacf0ba3ba
Sergey Shilin <sergey.shilin@phystech.edu>
2015-12-05 06:29:28 +0300

    Questionnaire activity, GeneratePassword activity

---
+++ app/src/main/java/android/com/smartgen/GeneratePassword.java
@@ -0,0 +1,24 @@
+package android.com.smartgen;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by snape on 05.12.15.
+ */
+public class GeneratePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.generate_password);
+
+    }
+
+    public String generatePassword(View v) {
+
+        return "";
+    }
+}
--- app/src/main/java/android/com/smartgen/Questionnaire.java
+++ app/src/main/java/android/com/smartgen/Questionnaire.java
@@ -1,15 +1,26 @@
 package android.com.smartgen;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TableLayout;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Questionnaire extends AppCompatActivity {
+
+    private Map<EditText, String> inputs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,34 +29,112 @@ public class Questionnaire extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        generateInputIds();
+        listenInputs();
+        listenButtons();
+    }
+
+    private void listenButtons() {
+        Button prev = (Button) findViewById(R.id.button_to_1_from_2);
+        Button next = (Button) findViewById(R.id.button_to_2_from_1);
+        Button save = (Button) findViewById(R.id.saveQuestionsButton);
+
+        prev.setOnClickListener(new Button.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                TableLayout table1 = (TableLayout) findViewById(R.id.table_1);
+                TableLayout table2 = (TableLayout) findViewById(R.id.table_2);
+
+                table2.setVisibility(View.GONE);
+                table1.setVisibility(View.VISIBLE);
+                table1.scrollTo(0, 0);
+            }
+        });
+
+        next.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TableLayout table1 = (TableLayout) findViewById(R.id.table_1);
+                TableLayout table2 = (TableLayout) findViewById(R.id.table_2);
+
+                table1.setVisibility(View.GONE);
+                table2.setVisibility(View.VISIBLE);
+                table2.scrollTo(0, 0);
+//                AlphaAnimation anim = new AlphaAnimation(1.0f, 0.0f);
+//                anim.setDuration(1000);
+//                anim.setRepeatMode(Animation.REVERSE);
+//                table1.startAnimation(anim);
+
+
             }
         });
     }
 
+    public void saveAnswers(View view) {
+        saveAnswersToStorage();
+
+        Intent myIntent = new Intent(Questionnaire.this, GeneratePassword.class);
+        Questionnaire.this.startActivity(myIntent);
+    }
+
+    private void saveAnswersToStorage() {
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        for (final Map.Entry<EditText, String> input : inputs.entrySet())
+            editor.putString(input.getKey().getText().toString(), input.getValue());
+
+        editor.commit();
+    }
+
+    private void generateInputIds() {
+        inputs = new HashMap<>();
+
+        inputs.put((EditText) findViewById(R.id.answer_1), "");
+        inputs.put((EditText) findViewById(R.id.answer_2), "");
+        inputs.put((EditText) findViewById(R.id.answer_3), "");
+        inputs.put((EditText) findViewById(R.id.answer_4), "");
+        inputs.put((EditText) findViewById(R.id.answer_5), "");
+        inputs.put((EditText) findViewById(R.id.answer_6), "");
+        inputs.put((EditText) findViewById(R.id.answer_7), "");
+        inputs.put((EditText) findViewById(R.id.answer_8), "");
+        inputs.put((EditText) findViewById(R.id.answer_9), "");
+        inputs.put((EditText) findViewById(R.id.answer_10), "");
+    }
+
+    private void listenInputs() {
+        for (final Map.Entry<EditText, String> input : inputs.entrySet()) {
+            input.getKey().addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    ProgressBar progress = (ProgressBar) findViewById(R.id.progressBar);
+
+                    if(input.getValue().isEmpty() && !String.valueOf(s).isEmpty())
+                        progress.setProgress((int) (progress.getProgress() + new Float(100) / inputs.size()));
+                    else if (!input.getValue().isEmpty() && String.valueOf(s).isEmpty())
+                        progress.setProgress((int) (progress.getProgress() - new Float(100) / inputs.size()));
+
+                    input.setValue(String.valueOf(s));
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_questionnaire, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
